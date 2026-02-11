de6e430d132baefb8c2d2131cfb776ee2d73a20a
SKYLINE2\GuilhermeGR <guilherme.gaspar@skyline.be>
2026-02-11 10:29:05 +0000

    ParameterModel thread safe and invoke changed events

--- Utils.UnitTestingFramework/Model/IParameterModel.cs
+++ Utils.UnitTestingFramework/Model/IParameterModel.cs
@@ -8,6 +8,11 @@
     public interface IParameterModel
     {
         /// <summary>
+        /// Occurs when the parameter value or timestamp changes.
+        /// </summary>
+        event EventHandler<ParameterModelChangedEventArgs> Changed;
+
+        /// <summary>
         /// Gets the parameter value.
         /// </summary>
         /// <value>
@@ -22,5 +27,12 @@
         /// The timestamp.
         /// </value>
         DateTime Timestamp { get; }
+
+        /// <summary>
+        /// Updates the parameter value and timestamp.
+        /// </summary>
+        /// <param name="value">The new value.</param>
+        /// <param name="timestamp">The timestamp.</param>
+        void Update(object value, DateTime? timestamp = null);
     }
 }
