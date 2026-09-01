2ea236842f57c5ab5ee7f073e995c800d61e54c5
Victor Scherpereel <victor.scherpereel@skyline.be>
2026-09-01 14:25:42 +0200

    Better structure

---
+++ Utils.UnitTestingFramework.DataMinerSystem.Common.Tests/Examples/Business_Code_Example_FindElementByName.cs
@@ -0,0 +1,32 @@
+﻿namespace Utils.UnitTestingFramework.Tests.DataMinerSystem.Examples
+{
+    using System;
+    using Skyline.DataMiner.Core.DataMinerSystem.Common;
+
+    internal static class Business_Code_Example_FindElementByName
+    {
+        public static IDmsElement FindElementByName(IDma dma, string elementName)
+        {
+            if (dma is null)
+            {
+                throw new ArgumentNullException(nameof(dma));
+            }
+
+            if (string.IsNullOrEmpty(elementName))
+            {
+                throw new ArgumentException("Element name cannot be null or empty.", nameof(elementName));
+            }
+
+            foreach (var element in dma.GetElements())
+            {
+                if (element.Name.Equals(elementName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return element;
+                }
+            }
+
+            // Element not found
+            return null;
+        }
+    }
+}
